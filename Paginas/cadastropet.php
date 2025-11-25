28e6bf340bcec4f4f74337d85b2b9ce7bcb14919
GuilhermeAlexandre07 <guilhermeale532@gmail.com>
2025-11-25 14:18:20 -0300

    link

--- Paginas/cadastropet.php
+++ Paginas/cadastropet.php
@@ -32,7 +32,7 @@ include ('../PHP/conexao.php');
                 <a href="comoajudar.php">Como ajudar</a>
 
                 <?php if (!isset($_SESSION['usuario_id'])): ?>
-                    <a href="Paginas/entrar.html" id="btn-entrar" class="botao-entrar">Entrar</a>
+                    <a href="entrar.html" id="btn-entrar" class="botao-entrar">Entrar</a>
                 <?php else: ?>
                     <div class="usuario-box" id="userMenu">
                         <img src="IMG/usuario/<?php echo $_SESSION['usuario_foto']; ?>" 
